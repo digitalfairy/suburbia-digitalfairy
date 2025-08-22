518944cd8bfd54999f790396677ec00324ecf2ff
Anita <jelicanita7@gmail.com>
2025-08-22 19:25:42 +0200

    Interactive Skateboard, Wavy Paths...

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,12 +1,21 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import { asImageSrc, Content } from "@prismicio/client";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
 
 import { Bounded } from "@/components/Bounded";
 import { Heading } from "@/components/Heading";
 import { ButtonLink } from "@/components/ButtonLink";
 import { WideLogo } from "./WideLogo";
 import { TallLogo } from "./TallLogo";
+import { InteractiveSkateboard } from "./InteractiveSkateboard";
+
+const DEFAULT_DECK_TEXTURE = "/skateboard/Deck.webp";
+const DEFAULT_WHEEL_TEXTURE = "/skateboard/SkateWheel1.png";
+const DEFAULT_TRUCK_COLOR = "#6F6E6A";
+const DEFAULT_BOLT_COLOR = "#6F6E6A";
 
 /**
  * Props for `Hero`.
@@ -16,17 +25,25 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 /**
  * Component for "Hero" Slices.
  */
-const Hero: FC<HeroProps> = ({ slice }) => {
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const deckTextureURL =
+    asImageSrc(slice.primary.skateboard_deck_texture) || DEFAULT_DECK_TEXTURE;
+  const wheelTextureURL =
+    asImageSrc(slice.primary.skateboard_wheel_texture) || DEFAULT_WHEEL_TEXTURE;
+  const truckColor =
+    slice.primary.skateboard_truck_color || DEFAULT_TRUCK_COLOR;
+  const boltColor = slice.primary.skateboard_bolt_color || DEFAULT_BOLT_COLOR;
+
   return (
-    <Bounded 
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
-      >
-        <div className="absolute inset-0 flex items-center pt-20">
-          <WideLogo className="w-full text-brand-purple hidden opacity-20 mix-blend-multiply lg:block" />
-          <TallLogo className="w-full text-brand-purple opacity-20 mix-blend-multiply lg:hidden" />
-        </div>
+    >
+      <div className="absolute inset-0 flex items-center pt-20">
+        <WideLogo className="w-full text-brand-purple hidden opacity-20 mix-blend-multiply lg:block" />
+        <TallLogo className="w-full text-brand-purple opacity-20 mix-blend-multiply lg:hidden" />
+      </div>
 
       <div className="absolute inset-0 mx-auto mt-24 grid max-w-6xl grid-rows-[1fr,auto] place-items-end px-6 ~py-10/16">
         <Heading className="relative max-w-2xl place-self-start">
@@ -36,7 +53,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
           <div className="max-w-[45ch] font-semibold ~text-lg/xl">
             <PrismicRichText field={slice.primary.body} />
           </div>
-         <ButtonLink
+          <ButtonLink
             field={slice.primary.button}
             icon="skateboard"
             size="lg"
@@ -47,7 +64,12 @@ const Hero: FC<HeroProps> = ({ slice }) => {
         </div>
       </div>
 
-      {/* Skateboard abs pos */}
+      <InteractiveSkateboard
+        deckTextureURL={deckTextureURL}
+        wheelTextureURL={wheelTextureURL}
+        truckColor={truckColor}
+        boltColor={boltColor}
+      />
     </Bounded>
   );
 };
